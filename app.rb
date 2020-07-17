328cb932205c7f473590b0d4afeadf99708d3eee
Rouan Thompson <rouan.thompson@gmail.com>
2020-07-16 20:21:37 -0400

    Done.

--- app.rb
+++ app.rb
@@ -2,5 +2,13 @@ require 'sinatra/base'
 
 class App < Sinatra::Base
 
+    #responds to a get request
+    get '/newteam' do
+        erb :newteam
+    end
 
+    #responds to a post request
+    post '/team' do
+        erb :team
+    end
 end
